6c2bd940b12fb17b6646d874754c667c7d9eec01
Tobias Wrigstad <tobias@jeepen.org>
2014-02-12 21:26:46 +0100

    Added string->string method

--- agent/Utils.java
+++ agent/Utils.java
@@ -5,13 +5,24 @@
 import org.cliffc.high_scale_lib.NonBlockingHashMap;
 public class Utils {
     private static volatile long counter = 0; 
-    private static final NonBlockingHashMap<String,Long> stringToInt = new NonBlockingHashMap<String,Long>();
-    public static long stringToInt(final String string) {
-	Long shorthand = stringToInt.get(string);
+    private static final NonBlockingHashMap<String,Long> stringToLong = new NonBlockingHashMap<String,Long>();
+    private static final NonBlockingHashMap<String,Long> stringToString = new NonBlockingHashMap<String,String>();
+
+    public static long stringToLong(final String string) {
+	final Long shorthand = stringToLong.get(string);
 	if (shorthand == null) {
 	    final long counter = ++Utils.counter;
-	    stringToInt.put(string, counter);
+	    stringToLong.put(string, counter);
 	    return counter;
+	}
+	return shorthand;
+    }
+
+    public static String stringToString(final String string) {
+	String shorthand = stringToLong.get(string);
+	if (shorthand == null) {
+	    shorthand = "" + ++Utils.counter; // UGLY HACK!!!
+	    stringToLong.put(string, shorthand); 
 	}
 	return shorthand;
     }
