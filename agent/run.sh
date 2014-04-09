272480608709f36d4ea89fc441b9f8a4acef684d
zamba <erikosam@gmail.com>
2014-04-09 16:53:00 +0200

    parameter

--- agent/run.sh
+++ agent/run.sh
@@ -5,7 +5,7 @@
 CP="-Xbootclasspath/p:/home/erik/java-alias-agent/agent/lib/asm-all-4.2.jar:/home/erik/java-alias-agent/agent/lib/awio.jar"
 
 
-java $CP -agentpath:./lib/testagent.so Rocket
+java $CP -agentpath:./lib/testagent.so $1
 
 
 
